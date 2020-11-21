7ad08221d9b3b6c2e8b5bce269f32749d31faca7
ecv2115 <ecv.eren@gmail.com>
2020-11-20 23:31:28 -0500

    modified seed file

--- seed.js
+++ seed.js
@@ -3,19 +3,21 @@ const { addPhoto } = require('./controllers/controllers');
 const Photo = require('./models/Photo');
 const dbConnection = require('./db/connection');
 const restaurant_id_arr = [1, 2, 3, 4, 5]
+const picModifiers = ['sushi', 'burrito', 'chicken', 'ramen', 'dessert']
 
 const createPhotoRecord = () => {
   let randomRestaurant_id = Math.floor(Math.random()* 5)
   let randomFriends = Math.floor(Math.random()* 101);
   let randomReviews = Math.floor(Math.random()* 101);
   let record = {
-    "url": `${faker.image.food()}`,
+    "url": `https://loremflickr.com/320/240/${picModifiers[randomRestaurant_id]}`,
     "description": `${faker.lorem.sentence()}`,
     "date": `${faker.date.past()}`,
     "user": {
       "username": `${faker.internet.userName()}`,
       "friends": randomFriends,
-      "reviews": randomReviews
+      "reviews": randomReviews,
+      "avatar": `${faker.image.avatar()}`
     },
     "restaurant_id": restaurant_id_arr[randomRestaurant_id]
   }
