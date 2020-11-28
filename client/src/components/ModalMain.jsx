5c0eb6dbdb76df105691c18c95ab9d9f86a28c40
ecv2115 <ecv.eren@gmail.com>
2020-11-27 21:14:31 -0500

    added click actions to buttons

--- client/src/components/ModalMain.jsx
+++ client/src/components/ModalMain.jsx
@@ -5,7 +5,7 @@ const ModalMain = (props) => {
   return(
     <div className="modal-column-left-main">
       <img className="modal-column-left-main-photo"src={props.modalPhoto.url}></img>
-      <ModalInfo info={props.modalPhoto} helpActive={props.helpActive} helpActiveButton={props.helpActiveButton}/>
+      <ModalInfo info={props.modalPhoto} helpfulActive={props.helpfulActive} notHelpfulActive={props.notHelpfulActive} reportActive={props.reportActive} toggleHelpfulActive={props.toggleHelpfulActive} toggleNotHelpfulActive={props.toggleNotHelpfulActive} toggleReportActive={props.toggleReportActive}/>
   </div>
   )
 }
