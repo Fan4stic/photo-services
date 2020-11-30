589eb7c4e3d1a9ddee3b819800897da8a537f3ce
ecv2115 <ecv.eren@gmail.com>
2020-11-29 21:40:38 -0500

    changed track display from five to four

--- client/src/components/Track.jsx
+++ client/src/components/Track.jsx
@@ -4,7 +4,7 @@ import Thumbnail from './Thumbnail.jsx';
 const Track =(props) => {
   return(
     <div className="row">
-      {props.photos.map((photo, index) => <Thumbnail photo={photo} key={index} openModal={props.openModal} setMainModal={props.setMainModal}/>).slice(0,5)}
+      {props.photos.map((photo, index) => <Thumbnail photo={photo} key={index} openModal={props.openModal} setMainModal={props.setMainModal}/>).slice(0,4)}
     </div>
   )
 }
